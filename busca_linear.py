22c90f9756955692abc018bdd3894765bfeaf27a
Victor Emanuel <victor_emanuell@yahoo.com.br>
2021-08-30 22:40:07 -0300

    Primeiro commit!

---
+++ busca_linear.py
@@ -0,0 +1,21 @@
+
+#Busca Linear
+
+def busca_linear(A, n, x):
+
+    resposta = -1
+
+    for i in range(0, n):
+        if A[i] == x: resposta = i
+
+    return resposta
+
+#Entradas
+A= [10,9,8,7,6,5,4,3,2,1]
+n=len(A)
+x=3
+
+#Saída
+saida = busca_linear(A, n, x)
+
+print(saida)
